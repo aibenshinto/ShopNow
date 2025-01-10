b9404b59f3a622ea4084e1fbee69b6669589ae32
aibenshinto <aibenshinto@gmail.com>
2025-01-10 11:39:56 +0530

    review created

--- shopnow_project/urls.py
+++ shopnow_project/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', include('authentication_app.urls')),
     path('accounts/', include('allauth.urls')),
     path('product_app/',include('product_app.urls')),
+    path('review/',include('reviews.urls')),
     path('', include('order.urls')),  # Include the orders app URLs
     path('', include('cart_app.urls')),  # Replace 'your_app_name' with your app's name
     path('api/', include('shippinaddress.urls')),
